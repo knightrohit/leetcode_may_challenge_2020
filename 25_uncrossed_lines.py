60a8c69cba649b2618ee9769fcd1b248c47d641c
Rohit <knight.rohit@gmail.com>
2020-05-25 12:31:51 -0500

    using manual dict

--- 25_uncrossed_lines.py
+++ 25_uncrossed_lines.py
@@ -2,6 +2,7 @@
 Time/Space complexity = O(N)
 '''
 
+# method1 - use lru cache
 from functools import lru_cache
 class Solution:
     def maxUncrossedLines(self, A: List[int], B: List[int]) -> int:
@@ -19,3 +20,26 @@ class Solution:
                 return max(lcd(i, j+1),  lcd(i+1, j))
             
         return lcd(0,0)
+
+
+#method2 - manual dict cache
+class Solution:
+    def maxUncrossedLines(self, A, B):
+        memo = {}
+
+        def lcd(i, j):
+            if i >= len(A) or j >= len(B):
+                return 0
+            
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if A[i] == B[j]:
+                memo[(i, j)] = 1 + lcd(i+1, j+1)
+
+            else:
+                memo[(i, j)] = max(lcd(i+1, j), lcd(i, j+1))
+
+            return memo[(i, j)]    
+
+        return lcd(0, 0)
